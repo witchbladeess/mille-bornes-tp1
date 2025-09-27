4899623a97613433ec34c75d9b0f03c440910aec
witchbladeess <filiushkina.s@gmail.com>
2025-09-27 15:27:21 +0200

    test piocher + test iterateur

--- mille_bornes/src/testsFonctionnels/TestSabot.java
+++ mille_bornes/src/testsFonctionnels/TestSabot.java
@@ -1,5 +1,7 @@
 package testsFonctionnels;
 
+import java.util.Iterator;
+
 import cartes.Carte;
 import cartes.JeuDeCartes;
 import jeu.Sabot;
@@ -10,9 +12,19 @@ public class TestSabot {
 		Carte[] toutes = jeu.donnerCartes();
 		Sabot sabot = new Sabot(toutes);
 		
+		System.out.println("TEST PIOCHER\n");
 		while(!sabot.estVide()) {
 			Carte carte = sabot.piocher();
 			System.out.println("je pioche " + carte);
+		}
+		
+		sabot = new Sabot(jeu.donnerCartes());
+		System.out.println("\nTEST ITERATEUR\n");
+		Iterator<Carte> iter = sabot.iterator();
+		while(iter.hasNext()) {
+			Carte c = iter.next();
+			System.out.println("je pioche " + c);
+			iter.remove();
 		}
 	}	
 }
