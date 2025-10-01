1e54b944a5fae6e63eec24ce9947fcff54ce1ade
witchbladeess <filiushkina.s@gmail.com>
2025-10-01 09:45:20 +0200

    exo 2.a -extraire

--- mille_bornes/src/utils/GestionCartes.java
+++ mille_bornes/src/utils/GestionCartes.java
@@ -1,5 +1,29 @@
 package utils;
 
-public class GestionCartes {
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 
+public class GestionCartes {
+	private static final Random random = new Random();
+	
+	// : utilise List directement
+	public static <T> T extraire(List<T> liste) {
+		if(liste.isEmpty()) throw new IllegalArgumentException("liste vide");
+		int index = random.nextInt(liste.size());
+		return liste.remove(index);
+	}
+	
+	// version ListIterator
+	 public static <T> T extraireAvecIterateur(List<T> liste) {
+		if(liste.isEmpty()) throw new IllegalArgumentException("Liste vide");
+		int index = random.nextInt(liste.size());
+		boolean found = false;
+		for(ListIterator<T> iterator = liste.listIterator(); iterator.hasNext() && !found;) {
+			
+		}
+		return null;
+		
+	 }
+	
 }
