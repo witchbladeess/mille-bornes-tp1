bdd68527b62a89630263319cd923873232b4fae4
witchbladeess <filiushkina.s@gmail.com>
2025-10-04 18:33:43 +0200

    generics

--- mille_bornes/src/utils/GestionCartes.java
+++ mille_bornes/src/utils/GestionCartes.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
@@ -19,11 +21,56 @@ public class GestionCartes {
 		if(liste.isEmpty()) throw new IllegalArgumentException("Liste vide");
 		int index = random.nextInt(liste.size());
 		boolean found = false;
+		int i = 0;
+		T element = null;
 		for(ListIterator<T> iterator = liste.listIterator(); iterator.hasNext() && !found;) {
-			
+			T courant = iterator.next();
+			if(i == index) {
+				element = courant;
+				iterator.remove();
+				found = true;
+			}
 		}
-		return null;
-		
+		return element;		
 	 }
 	
+	 public static <T> List<T> melanger(List <T> liste){
+		 List <T> melangee = new ArrayList<T>(); 
+		 while(!liste.isEmpty()) {
+			 T element = extraire(liste);
+			 melangee.add(element);
+		 }
+		 return melangee;
+	 }
+	 
+	 public static <T> boolean verifierMelange(List <T> l1, List<T> l2) {
+		 if(l1.size() != l2.size()) return false;
+		 for(T element : l1) {
+			 int freq1 = Collections.frequency(l1, element);
+			 int freq2 = Collections.frequency(l2, element);
+			 if(freq1!=freq2) return false;
+		 }
+		 return true;
+	 }
+	 
+	 public static <T> List <T> rassembler(List<T> liste){
+		 List<T> result = new ArrayList<T>();
+		 for(T element : liste) {
+			 int lastindex = result.lastIndexOf(element);
+			 if(lastindex == -1) {
+				 result.add(element);
+			 } else {
+				 result.add(lastindex+1, element);
+			 }
+			 
+		 }
+		 return result;
+	 }
+	 public static <T> boolean verifierRassemblement(List<T> liste) {
+		 if (liste.size() <= 1) return true;
+		 ListIterator<T> iterator1 = liste.listIterator();
+		 
+		 
+	 }
+	 
 }
